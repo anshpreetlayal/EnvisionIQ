cc3bd5cc4ec67f7fda320054adc1c7fd912adab5
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-05 19:01:57 -0500

    adding functionality

---
+++ reducers/userReducer.js
@@ -0,0 +1,27 @@
+const initialState = {
+    user: null,
+    preferences: {},
+    loading: false,
+    error: null,
+  };
+  
+  const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'SET_USER':
+        return { ...state, user: action.payload };
+      case 'SET_PREFERENCES':
+        return { ...state, preferences: action.payload };
+      case 'UPDATE_PROFILE_LOADING':
+        return { ...state, loading: true, error: null };
+      case 'UPDATE_PROFILE_SUCCESS':
+        return { ...state, user: action.payload, loading: false };
+      case 'UPDATE_PROFILE_ERROR':
+        return { ...state, loading: false, error: action.payload };
+      // Add more cases as needed for user-related actions
+      default:
+        return state;
+    }
+  };
+  
+  export default userReducer;
+  
