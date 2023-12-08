be3462c070dba629d3efbc3a2e603a86e6366872
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-08 13:00:25 -0500

    editing

--- Components/CategoryScreen.js
+++ Components/CategoryScreen.js
@@ -1,31 +1,50 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const CategoryScreen = ({ navigation }) => {
   const handleCategorySelection = (category) => {
     navigation.navigate('Home', { category });
   };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 20 }}>Choose a Category</Text>
-      <View style={{ marginVertical: 20 }}>
+    <View style={styles.container}>
+      <Text style={styles.title}>Choose a Category</Text>
+      <View style={styles.buttonContainer}>
         <Button title="Inspirational" onPress={() => handleCategorySelection('inspirational')} />
       </View>
-      <View style={{ marginVertical: 20 }}>
+      <View style={styles.buttonContainer}>
         <Button title="Motivational" onPress={() => handleCategorySelection('motivational')} />
       </View>
-      <View style={{ marginVertical: 20 }}>
+      <View style={styles.buttonContainer}>
         <Button title="Success" onPress={() => handleCategorySelection('success')} />
       </View>
-      <View style={{ marginVertical: 20 }}>
+      <View style={styles.buttonContainer}>
         <Button title="Love" onPress={() => handleCategorySelection('love')} />
       </View>
-      <View style={{ marginVertical: 20 }}>
+      <View style={styles.buttonContainer}>
         <Button title="Wisdom" onPress={() => handleCategorySelection('wisdom')} />
       </View>
       /* Add more buttons for different categories */
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    width: '60%',
+  },
+});
 
 export default CategoryScreen;
