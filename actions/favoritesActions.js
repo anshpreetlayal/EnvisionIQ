9398c4ae3d0783fa0d3b9a93ce4f2111669d611f
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-08 19:14:02 -0500

    added actions

--- actions/favoritesActions.js
+++ actions/favoritesActions.js
@@ -2,3 +2,6 @@ export const saveFavoriteQuote = (quote) => ({
     type: "SAVE_FAVORITE_QUOTE",
     payload: quote,
   });
+  export const clearSavedQuotes = () => ({
+    type: "CLEAR_SAVED_QUOTES",
+  });
