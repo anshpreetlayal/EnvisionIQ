a698569c108507447f46a233eb3901c036e7dd3b
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-11 12:38:33 -0500

    editing

--- App.js
+++ App.js
@@ -1,39 +1,56 @@
-import React from "react";
-import {
-  Text,
-  SafeAreaView,
-  StyleSheet,
-  Button,
-  ActivityIndicator,
-} from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../Components/HomeScreen';
+import CategoryScreen from '../Components/CategoryScreen';
+import JournalScreen from '../Components/JournalScreen';
+import SavedQuotesScreen from '../Components/SavedQuotesScreen';
+import SettingsScreen from '../Components/SettingsScreen';
+import LoginScreen from '../AuthScreens/LoginScreen';
+import SignUpScreen from '../AuthScreens/SignUpScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Welcome to EnvisionIQ</Text>
-      <ActivityIndicator size="large" color="purple" animating={true} />
-      <Button
-        title="Get quote"
-        onPress={() => {
-          console.log("Button pressed");
-        }}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Home' }}
+        />
+        <Stack.Screen
+          name="Category"
+          component={CategoryScreen}
+          options={{ title: 'Find your type of quote' }}
+        />
+        <Stack.Screen
+          name="Journal"
+          component={JournalScreen}
+          options={{ title: 'How do you feel today?' }}
+        />
+        <Stack.Screen
+          name="SavedQuotes"
+          component={SavedQuotesScreen}
+          options={{ title: 'Your encyclopedia of inspiration' }}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{ title: 'Settings' }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: 'Have an account? Login' }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={SignUpScreen}
+          options={{ title: 'Don\'t have an account? Register' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#ecf0f1",
-    padding: 8,
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-});
--- Components/api.js
+++ Components/api.js
@@ -1,28 +1,47 @@
+import React, { useEffect, useState } from "react";
 import { Text } from "react-native";
-import { useEffect, useState } from "react";
 
 function GetQuote(props) {
   const [quote, setQuote] = useState("");
   const [category, setCategory] = useState("inspirational");
 
-  const url = "https://api.api-ninjas.com/v1/quotes?category=" + category;
-  const apikey = { "X-Api-Key": "Nc1JqwaxxhshJ/808pw3LA==mZ17URUr5TiD6RjX" };
+  const url = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
+  const apikey = "Nc1JqwaxxhshJ/808pw3LA==mZ17URUr5TiD6RjX";
 
   useEffect(() => {
-    if (!(props == undefined)) {
+    if (props && props.category) {
       setCategory(props.category);
     }
 
-    fetch(url, {
-        method: "GET",
-        headers: apikey,
-      })
-        .then((res) => res.json())
-        .then((json) => setQuote(json[0].quote));
-    }, []);
-  
-    return <Text>{quote}</Text>;
-  }
-  
-  export default GetQuote;
-  
+    const fetchQuote = async () => {
+      try {
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            "X-Api-Key": apikey,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch');
+        }
+
+        const json = await response.json();
+        if (json && json.length > 0 && json[0].quote) {
+          setQuote(json[0].quote);
+        } else {
+          setQuote("No quote available");
+        }
+      } catch (error) {
+        console.error("Error fetching quote:", error);
+        setQuote("Unable to fetch quote at the moment");
+      }
+    };
+
+    fetchQuote();
+  }, [props, url, apikey, category]);
+
+  return <Text>{quote}</Text>;
+}
+
+export default GetQuote;
