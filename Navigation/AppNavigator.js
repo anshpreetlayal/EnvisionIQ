b4f7e5577bd2a918ee6d2253a7782570bbc709ce
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-15 18:36:19 -0500

    edits

--- Navigation/AppNavigator.js
+++ Navigation/AppNavigator.js
@@ -1,18 +1,44 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { Text, SafeAreaView, StyleSheet, View, Button } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './components/HomeScreen';
+import CategoryScreen from './components/CategoryScreen';
+import JournalScreen from './components/JournalScreen';
+import SavedQuotesScreen from './components/CategoryScreen';
+import SettingsScreen from './components/SettingsScreen'; 
+import TaskManager from './components/TaskManagerScreen';
+import LoginScreen from './AuthScreens/LoginScreen';
+import SignUpScreen from './AuthScreens/SignUpScreen';
+const Stack = createNativeStackNavigator();
 
-import LoginScreen from './LoginScreen';
-import SignUpScreen from './SignUpScreen'; 
-const AppNavigator = createStackNavigator(
-  {
-    Login: LoginScreen,
-    SignUp: SignUpScreen,
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Category" component={CategoryScreen} />
+        <Stack.Screen name="JournalScreen" component={JournalScreen} />
+        <Stack.Screen name="SavedQuotes" component={SavedQuotesScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="TaskManager" component={TaskManager} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  {
-    initialRouteName: 'Login',
-    headerMode: 'none',
-    
-  }
-);
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
 
-export default createAppContainer(AppNavigator);
+export default App;
