79ae65b80b8df2d4dd84a4ab057718e738aac25f
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-15 18:37:07 -0500

    editing

--- reducers/favoritesReducer.js
+++ reducers/favoritesReducer.js
@@ -1,22 +1,21 @@
 const initialState = {
-    favoriteQuotes: [],
-    loading: false,
-    error: null,
-  };
-  
-  const favoriteQuotesReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'SAVE_FAVORITE_QUOTE':
-        return { ...state, favoriteQuotes: [...state.favoriteQuotes, action.payload] };
-      case 'REMOVE_FAVORITE_QUOTE':
-        return {
-          ...state,
-          favoriteQuotes: state.favoriteQuotes.filter(quote => quote.id !== action.payload.id),
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default favoriteQuotesReducer;
-  
+  favoriteQuotes: [],
+  loading: false,
+  error: null,
+};
+
+const favoriteQuotesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SAVE_FAVORITE_QUOTE':
+      return { ...state, favoriteQuotes: [...state.favoriteQuotes, action.payload] };
+    case 'REMOVE_FAVORITE_QUOTE':
+      return {
+        ...state,
+        favoriteQuotes: state.favoriteQuotes.filter(quote => quote.id !== action.payload.id),
+      };
+    default:
+      return state;
+  }
+};
+
+export default favoriteQuotesReducer;
